f9c3cbec4e520bd518e229f381b3b3e4f4ddc611
Wojciech Slabosz <230000@edu.p.lodz.pl>
2022-03-27 15:58:52 +0200

    teacher form done

--- src/components/Teacher/TeacherForm.tsx
+++ src/components/Teacher/TeacherForm.tsx
@@ -1,47 +1,75 @@
-import React from "react"
+import React, { useState } from "react"
 import { Teacher } from "../../types/Teacher"
 import { TextInput, Button, Group, Box } from "@mantine/core"
 import { useForm } from "@mantine/form"
 import axios from "axios"
+import { useNotifications } from "@mantine/notifications"
+import { Check } from "tabler-icons-react"
 
 export const TeacherForm: React.FC = () => {
-  const URL = "http://localhost:3567/api/teacher"
+  const URL = "http://localhost/api/teacher"
+  const notifications = useNotifications()
+  const [error, setError] = useState("")
 
-  const form = useForm<Teacher>({
+  const teacherForm = useForm<Teacher>({
     initialValues: {
-      first_name: "",
-      last_name: "",
+      firstName: "",
+      lastName: "",
     },
 
     validate: (values) => ({
-      first_name: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$/.test(values.first_name)
+      first_name: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$/.test(values.firstName)
         ? null
         : "Niepoprawne imię",
-      last_name: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$/.test(values.last_name)
+      last_name: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż]+$/.test(values.lastName)
         ? null
         : "Niepoprawne nazwisko",
     }),
   })
 
   const handleSubmit = (teacherData: Teacher) => {
-    const response: Promise<Teacher> = axios.post(URL, { teacherData })
+    axios
+      .post(
+        URL,
+        { teacherData },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "Allow-Origin": "*",
+          },
+        }
+      )
+      .catch((err) => setError(err.message))
+    notifications.showNotification({
+      title: `${
+        error
+          ? `Nie udało się dodać nauczyciela!`
+          : "Udało się dodać nauczyciela!"
+      }`,
+      autoClose: 3000,
+      icon: <Check size={18} />,
+      color: "green",
+      message: error
+        ? `Nie udało się dodać nauczyciela ${teacherForm.values.firstName} ${teacherForm.values.lastName}`
+        : `Udało się dodać nauczyciela ${teacherForm.values.firstName} ${teacherForm.values.lastName}`,
+    })
   }
 
   return (
     <Box sx={{ maxWidth: 400 }} mx="auto">
-      <form onSubmit={form.onSubmit(handleSubmit)}>
+      <form onSubmit={teacherForm.onSubmit(handleSubmit)}>
         <TextInput
           required
           label="Imię nauczyciela"
           placeholder="Imię"
-          {...form.getInputProps("first_name")}
+          {...teacherForm.getInputProps("firstName")}
         />
 
         <TextInput
           required
           label="Nazwisko nauczyciela"
           placeholder="Nazwisko"
-          {...form.getInputProps("last_name")}
+          {...teacherForm.getInputProps("lastName")}
         />
 
         <Group position="right" mt="md">
--- src/types/Teacher.tsx
+++ src/types/Teacher.tsx
@@ -1,4 +1,4 @@
 export type Teacher = {
-  first_name: string
-  last_name: string
+  firstName: string
+  lastName: string
 }
